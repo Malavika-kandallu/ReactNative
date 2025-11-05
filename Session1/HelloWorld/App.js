b12fa4403e525f2dc9732a9e3cc2cfbccbcb6d3d
malavika.kr <malavika.kr@indium.tech>
2025-11-05 14:25:19 +0530

    Transaction List page for the application using Flat List and Scroll View

--- Session1/HelloWorld/App.js
+++ Session1/HelloWorld/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ScrollView} from 'react-native';
 
 export default function App() {
   return (
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
